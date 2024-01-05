fda42530945c7f66c2e78eedec076d304aa1f7e2
igors2004 <is310810@student.polsl.pl>
2024-01-05 17:21:51 +0100

    Create snake.py

---
+++ SNAKEAPP/snake.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import random
+
+
+class Snake:
+    pass
+class Food:
+    pass
+def nextturn():
+    pass
+def change_direction(new direction):
+    pass
+def check_collision():
+    pass
+def game_over():
+    pass
