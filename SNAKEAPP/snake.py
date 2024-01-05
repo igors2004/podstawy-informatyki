5606296fbff5c83decdfd4f97a117c4e1e11d49a
igors2004 <is310810@student.polsl.pl>
2024-01-05 17:48:16 +0100

    Update snake.py

--- SNAKEAPP/snake.py
+++ SNAKEAPP/snake.py
@@ -1,6 +1,16 @@
 from tkinter import *
 import random
 
+GAME_WIDTH = 700 # STALE
+GAME_HEIGHT = 700
+SPEED = 60
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#3246a8" #NIEBIESKI
+FOOD_COLOR = "#a89e32" #ZOLTY
+BACKGROUND_COLOR= "#231036" #FIOLETOWY
+
+
 
 class Snake:
     pass
@@ -8,9 +18,24 @@ class Food:
     pass
 def nextturn():
     pass
-def change_direction(new direction):
+def change_direction(new_direction):
     pass
 def check_collision():
     pass
 def game_over():
     pass
+
+
+
+window=Tk()
+window.title("SNAKE GAME")
+
+
+score = 0
+direction = 'down'
+label = Label(window, text="Score: {}".format(score, font=('consolas', 100)))
+label.pack()
+
+
+
+window.mainloop()
