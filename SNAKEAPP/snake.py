d2a519cfa741cade2427203367ec04859cfe0785
igors2004 <is310810@student.polsl.pl>
2024-01-06 13:48:11 +0100

    Update snake.py

--- SNAKEAPP/snake.py
+++ SNAKEAPP/snake.py
@@ -1,22 +1,31 @@
 from tkinter import *
 import random
 
-GAME_WIDTH = 700 # STALE
+GAME_WIDTH = 700  # STALE
 GAME_HEIGHT = 700
 SPEED = 60
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "#3246a8" #NIEBIESKI
-FOOD_COLOR = "#a89e32" #ZOLTY
-BACKGROUND_COLOR= "#231036" #FIOLETOWY
+SNAKE_COLOR = "#3246a8"  # NIEBIESKI
+FOOD_COLOR = "#a89e32"  # ZOLTY
+BACKGROUND_COLOR = "#231036"  # FIOLETOWY
 
 
 
 class Snake:
     pass
 class Food:
-    pass
-def nextturn():
+    def __init__(self):
+
+        x = random.randrange(0, (GAME_WIDTH / SPACE_SIZE))
+        y = random.randint(0, GAME_HEIGHT)
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = "food")
+
+
+def next_turn():
     pass
 def change_direction(new_direction):
     pass
@@ -48,6 +57,11 @@ x = int((screen_width - window_width) / 2)
 y = int((screen_height - window_height) / 2)
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
 
 
 window.mainloop()
