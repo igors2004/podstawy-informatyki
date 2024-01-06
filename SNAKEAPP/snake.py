92d16882cbd2a7bbc58de65d5220fec2345a83ad
igors2004 <is310810@student.polsl.pl>
2024-01-06 12:29:06 +0100

    Update snake.py

--- SNAKEAPP/snake.py
+++ SNAKEAPP/snake.py
@@ -33,9 +33,21 @@ window.title("SNAKE GAME")
 
 score = 0
 direction = 'down'
-label = Label(window, text="Score: {}".format(score, font=('consolas', 100)))
+label = Label(window, text="Score: {}".format(score), font=('consolas', 100))
 label.pack()
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
 
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width - window_width) / 2)
+y = int((screen_height - window_height) / 2)
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 
 window.mainloop()
